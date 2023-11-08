5f8596e47a35a037cbe48c06fa1ca28c4f71bd8e
Savva <sav.van.ivan3@mail.ru>
2023-11-08 18:55:32 +0300

    HW_3

--- src/com/teachmeskills/lesson3/HW3/Task7.java
+++ src/com/teachmeskills/lesson3/HW3/Task7.java
@@ -7,19 +7,29 @@ public class Task7 {
         Scanner scanner = new Scanner(System.in);
         int numberOfWeek = scanner.nextInt();
         switch (numberOfWeek) {
-          case   1 : System.out.println("Monday");
+          case 1 : System.out.println("January ");
             break;
-            case   2 : System.out.println("Tuesday");
+            case 2 : System.out.println("February");
                 break;
-            case   3 : System.out.println("Wednesday");
+            case 3 : System.out.println("March");
                 break;
-            case   4 : System.out.println("Thursday");
+            case 4 : System.out.println("April");
                 break;
-            case   5 : System.out.println("Friday");
+            case 5 : System.out.println("May");
                 break;
-            case   6 : System.out.println("Saturday");
+            case 6 : System.out.println("June");
                 break;
-            case   7 : System.out.println("Sunday");
+            case 7 : System.out.println("July");
+                break;
+            case 8 : System.out.println("August");
+                break;
+            case 9 : System.out.println("September");
+                break;
+            case 10 : System.out.println("October");
+                break;
+            case 11 : System.out.println("November");
+                break;
+            case 12 : System.out.println("December");
                 break;
             default:
                 System.out.println("there is no such day");
